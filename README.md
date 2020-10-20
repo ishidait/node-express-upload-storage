3f861b121f3f2226f5232ad5b0089e0f5ff015a4
Makoto Ishida <makotoishida@gmail.com>
2020-10-19 23:06:07 -1000

    Separate Dockerfile for production and development.

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # node-express-upload-storage
 
 An example Node.js app that receives uploaded files and stores them into cloud storage.
+
+# Build production container image
+
+> docker build -t uploadtest .
+
+# Run production container
+
+> docker run -it --rm -p3000:3000 --env-file .env uploadtest
+
+# Develop with docker-compose
+
+> docker-compose up -d
+
+Open http://localhost:3000 in a browser.
+
+> docker-compose down
+
+# Develop with VSCode Remote Container extension
+
+Open this folder with VSCode, then select "Remote-Containers: Reopen in Container" menu. Once open in the container, go to integrated terminal, then run:
+
+> npm run dev
+
+Open http://localhost:3000 in a browser.
+
+## Note:
+
+You should create a .env file and put your access keys etc in there before running the container.
--- src/upload/upload.router.ts
+++ src/upload/upload.router.ts
@@ -1,20 +1,38 @@
 import path from 'path';
 import express from 'express';
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import * as AWS from 'aws-sdk';
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  region: 'us-west-1',
+});
+const bucketName = process.env.AWS_S3_BUCKET ?? '';
 
 export const uploadRouter = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, './uploads/');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//   },
+// });
 
 const upload = multer({
-  storage: storage,
+  storage: multerS3({
+    s3: s3,
+    bucket: bucketName,
+    metadata: (req, file, cb) => {
+      cb(null, { appName: 'node-express-upload-storage' }); // Put any metadata here.
+    },
+    key: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    },
+  }),
   limits: { fileSize: 10 * 1024 * 1024, files: 10 }, // Max. 10MB x 10 files
 });
 
